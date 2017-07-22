f8c77dd21ca5e03c0c2dbadf42552fc1d7f32379
petegee <peteg@hotmail.co.nz>
2017-07-22 15:47:16 +1200

    added concouse yml file added "tests"

---
+++ Tests/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void ProveThatItIsMint()
+        {
+            Assert.Pass("your shit is not whack!");
+        }
+    }
+}
